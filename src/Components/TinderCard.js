21d18399403976f6786d29c850ad5d5bbefb529c
myk245 <myk245@nyu.edu>
2021-01-18 18:56:15 -0500

    placeholder tinder card

--- src/Components/TinderCard.js
+++ src/Components/TinderCard.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 function TinderCard() {
    return (
-      <div>
-         <h1>Tinder Card</h1>
+      <div id="tinder-card">
+         <h1>Fake Name</h1>
+         <img className="profile-image" src="http://via.placeholder.com/350x350
+"></img>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nostrum suscipit deserunt ea placeat dolorum eveniet. Vero, odio aspernatur debitis reprehenderit autem quam quae voluptate nesciunt eveniet, ipsum cupiditate velit vitae quod veritatis accusantium impedit consequatur. Optio eaque, quaerat, veritatis accusantium vel dolorum error sequi nobis aspernatur sunt alias numquam.</p>
       </div>
    )
 }
