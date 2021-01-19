b6fdd153ddb2ea6dcf4c1adf1fbc474e1a55cc44
myk245 <myk245@nyu.edu>
2021-01-18 22:33:41 -0500

    clean up tinder card

--- src/Components/TinderCard.js
+++ src/Components/TinderCard.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 function TinderCard() {
    return (
-      <div id="tinder-card">
+      <div className="container">
+         <div id="tinder-card">
          <h1>Fake Name</h1>
          <img className="profile-image" src="http://via.placeholder.com/350x350
 "></img>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nostrum suscipit deserunt ea placeat dolorum eveniet. Vero, odio aspernatur debitis reprehenderit autem quam quae voluptate nesciunt eveniet, ipsum cupiditate velit vitae quod veritatis accusantium impedit consequatur. Optio eaque, quaerat, veritatis accusantium vel dolorum error sequi nobis aspernatur sunt alias numquam.</p>
+         </div>
       </div>
    )
 }
